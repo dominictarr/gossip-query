33c782e2e2a1cc319aaf3ddcc754913c2d1ed3c2
cel <cel@f/6sQ6d2CMxRUhLpspgGIulDxDCwYD7DzFzPNr7u5AU=.ed25519>
2017-09-27 12:37:45 -1000

    implement more

--- index.js
+++ index.js
@@ -30,49 +30,77 @@ what if we just started with local queries and results?
 module.exports = function (opts) {
   //opts has {check, process, isQuery, isResponse}
   var state = {}
+
+  var initialWeight = opts.initialWeight || -1
+  var increase = opts.increase || function (n) { return Number(n) + 1 }
+  var isRequest = opts.isRequest || function (value) { return typeof value === 'number' && value < 0 }
+  var isResponse = opts.isResponse || function (value) { return !isRequest(value) }
+
   function next () {
     throw new Error('not yet implemented')
   }
 
-  //loopy thing that checks the local store when new queries are added
   function onUpdate () {
     for(var k in state) {
-      if(!state[k].checked) {
+      //check the local store when new queries are added
+      if(!state[k].checked && !state[k].checking) {
+        state[k].checking = true
         opts.check(k, function (err, value) {
+          if(err) console.trace(err) // TODO: delete or reject query?
+          state[k].checking = false
           state[k].checked = true
           if(value && !state[k].value) {
             state[k].value = value
           }
-          onUpdate()
-        }
+        })
+      }
+
+      //process items received
+      if(state[k].value != null && !state[k].processing && !state[k].ready) {
+        state[k].processing = true
+        opts.process(k, state[k].value, function (err, value) {
+          if(err) console.trace(err) // TODO: reject query?
+          state[k].processing = false
+          state[k].ready = true
+          if(value && !state[k].value) {
+            state[k].value = value
+          }
+        })
       }
     }
   }
-
-  //loopy thing that processes items received
 
   return {
     state: state,
     createStream: function (peerId) {
       return {
         source: function (end, cb) {
+          if(end) {
+            if(end !== true) console.trace(peerId, end)
+            for(var k in state) {
+              // TODO: use hashlru so we don't have to use delete
+              delete state[k].respondedTo[peerId]
+              delete state[k].requestedFrom[peerId]
+            }
+            return
+          }
           //read the next pieces of data from the state object.
           ;(function read () {
             var data = {}
             for(var k in state) {
               if(
                 //ready means it's been processed,
-                //or we already had it locally.
+                //or we already had it locally (TODO).
                 state[k].ready &&
-                state[k].value != null &&
-                state[k].requestedBy[peerId]
-                !state[k].responded[peerId]
+                state[k].requestedBy[peerId] &&
+                !state[k].respondedTo[peerId]
               ) {
-                state[k].responded[peerId] = true
+                state[k].respondedTo[peerId] = true
                 data[k] = state[k].value
               }
               else if(
-                !state[k].ready && state[k].query &&
+                !state[k].ready &&
+                state[k].checked &&
                 !state[k].requestedFrom[peerId]
               ) {
                 state[k].requestedFrom[peerId] = true
@@ -86,11 +114,17 @@ module.exports = function (opts) {
         },
         sink: function (read) {
           read(null, function more (end, data) {
-            //TODO: if end, remove peerID from the responded, requested{From,By} properties
+            if(end) {
+              if(end !== true) console.trace(peerId, end)
+              for(var k in state) {
+                delete state[k].requestedBy[peerId]
+              }
+              return
+            }
             //process this message and possibly update the state.
             var update = false
             for(var k in data) {
-              if(isRequest(data[k]) {
+              if(isRequest(data[k])) {
                 //if we already have seen this query:
                 if(state[k]) {
                   state[k].requestedBy[peerId] = true
@@ -99,21 +133,21 @@ module.exports = function (opts) {
                 else {
                   state[k] = {
                     ready: false,
-                    query: false,
+                    checked: false,
+                    checking: false,
                     weight: increase(data[k]),
                     value: null,
                     requestedBy: {},
-                    requestedFrom: {}
-                    respondedTo: {},
+                    requestedFrom: {},
+                    respondedTo: {}
                   }
                   state[k].requestedBy[peerId] = true
                   update = true
                 }
               }
-              else if(isResponse(data[k]) {
+              else if(isResponse(data[k])) {
                 //if this is a response,
                 state[k].value = data[k]
-                state[k].processing = true
                 update = true
               }
             }
