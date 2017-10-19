c5b137ecf49921110abdabcbe71b98e8d0c9e6fb
Dominic Tarr <dominic.tarr@gmail.com>
2017-10-20 06:21:34 +1300

    test the simplest cases with streams

--- test/index.js
+++ test/index.js
@@ -1,23 +1,73 @@
-
 var GossipQuery = require('../')
+var pull = require('pull-stream')
 
 var tape = require('tape')
 
-var gq = GossipQuery({
-  check: function (_, cb) { cb(null, true) },
-  process: function (query, cb) { cb() },
-  isQuery: function (query) { return true },
-  isResponse: function (res) { return 'boolean' === typeof res }
-})
+function GQ (store) {
+  return GossipQuery({
+    check: function (key, cb) { cb(null, store[key]) },
+    process: function (key, value, cb) { cb(null, value) },
+    isQuery: function (key) { return true },
+    isResponse: function (value) { return null != value  }
+  })
+}
+
+/*
+all these tests are sync, because it makes it really
+easy to reason about the ordering of events and
+produce correct tests
+*/
 
 tape('simple', function (t) {
-
+  var gq = GQ({query: true})
   gq.query('query', function (err, value) {
     t.equal(value, true)
     t.notOk(err)
     t.end()
   })
+})
 
-  console.log(gq)
+tape('stream: send request', function (t) {
+  var gq = GQ({}), _value
+  gq.query('query', function (err, value) {
+    _value = value
+  })
+  t.notOk(_value)
+
+  var stream = gq.createStream(), a = [], read_cb
+
+  stream.sink(function (_, cb) {
+    read_cb = cb
+  })
+
+  stream.source(null, function (err, msg) {
+    if(err) throw err
+    t.deepEqual(msg, {query: -1})
+    read_cb(null, {query: 'result'})
+    t.equal(_value, 'result')
+    console.log(gq)
+    t.end()
+  })
+})
+
+tape('stream: send response', function (t) {
+  var gq = GQ({foo: 'bar'})
+
+  var stream = gq.createStream(), a = [], read_cb
+
+  stream.sink(function (_, cb) {
+    read_cb = cb
+  })
+
+  //send a request
+  read_cb(null, {foo: -1})
+
+  stream.source(null, function (err, msg) {
+    if(err) throw err
+    t.deepEqual(msg, {foo: 'bar'})
+    console.log(gq)
+    t.end()
+  })
 
 })
+
