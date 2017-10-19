c90465b6add240a23993da06d740ac81190233ef
Dominic Tarr <dominic.tarr@gmail.com>
2017-10-19 23:32:16 +1300

    tests

---
+++ test/index.js
@@ -0,0 +1,21 @@
+
+var GossipQuery = require('../')
+
+var tape = require('tape')
+
+var gq = GossipQuery({
+  check: function (_, cb) { cb(null, true) },
+  process: function (query, cb) { cb() },
+  isQuery: function (query) { return true },
+  isResponse: function (res) { return 'boolean' === typeof res }
+})
+
+tape('simple', function (t) {
+
+  gq.query('query', function (err, value) {
+    t.equal(value, true)
+    t.notOk(err)
+    t.end()
+  })
+
+})
